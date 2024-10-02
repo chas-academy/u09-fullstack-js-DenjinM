af50ad3627016e54cba222a0162c27764da0d875
Denjin <denjin.muratagic@chasacademy.se>
2024-10-02 15:20:02 +0200

    Fixed my frontend connection

--- book-library/frontend/src/App.js
+++ book-library/frontend/src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { AuthProvider } from './contexts/AuthContext';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import BookPage from './pages/BookPage/BookPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Switch>
-          <Route path="/login" component={LoginPage} />
-          <Route path="/" component={HomePage} />
-        </Switch>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/books" element={<BookPage />} />
+      </Routes>
+    </Router>
   );
 }
+
 
 export default App;
---
+++ book-library/frontend/src/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.css'; // Importerar CSS-filen
+
+const HomePage = () => {
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Welcome to the Book Library</h1>
+      <p className="home-description">Explore our collection of books.</p>
+    </div>
+  );
+};
+
+export default HomePage;
+
