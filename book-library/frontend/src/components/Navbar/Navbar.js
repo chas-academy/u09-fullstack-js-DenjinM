6e920aff416d7774eb1eaef9450728095137e80a
Denjin <denjin.muratagic@chasacademy.se>
2024-10-10 20:52:42 +0200

    fixed logo for my navbar

--- book-library/frontend/src/components/Navbar/Navbar.js
+++ book-library/frontend/src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import bookLogo from '../../images/book-logo.png'; // Uppdatera sökvägen till logotypen
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -11,7 +12,10 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <h1 className="navbar-logo">Book Library</h1>
+      <div className="navbar-logo-container">
+        <img src={bookLogo} alt="Book Logo" className="navbar-logo-img" />
+        <h1 className="navbar-logo">Book Library</h1>
+      </div>
       <span className="navbar-toggle" onClick={toggleMenu}>
         &#9776; {/* Hamburger-ikon */}
       </span>
